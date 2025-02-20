188266598e55022c35c474bff9084a654cfdde14
Devendra116 <chauhandevendra116@gmail.com>
2025-02-21 00:02:42 +0530

    add get market price api

--- src/controllers/market.ts
+++ src/controllers/market.ts
@@ -16,4 +16,25 @@ export const getAllMarkets = async (req: any, res: any) => {
     }
 }
 
+/**
+* @route GET /market/prices
+* @description Returns list of price for particular market.
+* @param {string} market_id - Market ID.
+* @returns {Object} JSON response containing list of price at specific timestamp.
+*/
+export const getMarketPrices = async (req: any, res: any) => {
+    try {
+        const { market_id } = req.params;
+        if (!market_id) {
+            return res.status(400).json({ success: false, error: "Missing market_id" });
+        }
+        let all_prices = await query(`SELECT outcome_prices as price, timestamp FROM market_price WHERE market_id = $1`, [market_id]);
+
+        return res.json({ success: true, prices: all_prices });
+    } catch (error: any) {
+        console.error("Error Fetching Market Prices:", error);
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
+
 
--- src/routes/market.ts
+++ src/routes/market.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
 import { createFarcasterMarket, scheduleFarcasterMarket, validateFarcasterMarket } from "../controllers/farcaster";
-import { getAllMarkets } from "../controllers/market";
+import { getAllMarkets, getMarketPrices } from "../controllers/market";
 
 
 const router = Router();
 
-router.get("/all",getAllMarkets)
+router.get("/all", getAllMarkets)
+router.get("/prices/:market_id", getMarketPrices)
 router.post("/farcaster/schedule", scheduleFarcasterMarket);
 router.post("/farcaster/validate", validateFarcasterMarket);
 router.post("/farcaster/create", createFarcasterMarket);
